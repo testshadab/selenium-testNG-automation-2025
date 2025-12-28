1b030cd1823d718e0c91bc15304276d1a60a5d0b
Shadab Shaikh <test.shadab@gmail.com>
2025-12-29 00:09:01 +0530

    Update ConfigReader.java

--- src/main/java/org/example/config/ConfigReader.java
+++ src/main/java/org/example/config/ConfigReader.java
@@ -7,8 +7,8 @@ import java.util.Properties;
 public class ConfigReader {
     private static Properties prop = new Properties();
 
-    public static void loadProperties(String fileName) throws IOException {
-        FileInputStream fis = new FileInputStream("src/main/resources/" + fileName + ".properties");
+    public static void loadProperties(String env) throws IOException {
+        FileInputStream fis = new FileInputStream("src/main/resources/" + env + ".properties");
         prop.load(fis);
     }
 
